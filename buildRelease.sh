8876d4ee08993fa549f8173434b64761f0326ae6
linuxgurugamer <linuxgurugamer@gmail.com>
2016-12-28 11:09:59 -0500

    Initial release

---
+++ buildRelease.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+v="NEBULA/NEBULA_Decals.version"
+major=`grep -m 1 MAJOR $v | cut -f2 -d':' | tr -d , | tr -d '[:space:]'`
+minor=`grep -m 1 MINOR $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+patch=`grep -m 1 PATCH $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+version="${major}.${minor}.${patch}"
+
+echo $version
+
+mkdir GameData
+cp -a NEBULA GameData/NEBULA
+
+#releasedir=/d/Users/jbb/release
+releasedir="."
+fname="${releasedir}/NEBULA_Decals-${version}.zip"
+zip -9r $fname GameData
+rm -fr GameData
