11b74a6e0d88507d6cc6e6562acdf2fa4adb5c0f
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-01-03 07:56:59 -0500

    updated build delted conflicting file

--- buildRelease.sh
+++ buildRelease.sh
@@ -3,7 +3,12 @@ v="NEBULA/NEBULA_Decals.version"
 major=`grep -m 1 MAJOR $v | cut -f2 -d':' | tr -d , | tr -d '[:space:]'`
 minor=`grep -m 1 MINOR $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
 patch=`grep -m 1 PATCH $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+build=`grep -m 1 BUILD $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
 version="${major}.${minor}.${patch}"
+if [ "$build" != "0" ]; then
+		version="${version}.${build}"
+fi
+
 
 echo $version
 
